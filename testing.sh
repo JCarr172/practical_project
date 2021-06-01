1b65b25c7deec978f74141748755802b7c5bbe56
James Carr <jcarr@qa.com>
2021-06-01 14:08:52 +0000

    Edited Jenkinsfile

--- testing.sh
+++ testing.sh
@@ -8,7 +8,7 @@ sudo apt-get install python3 python3-venv python3-pip -y
 #setup venv and install pip requirements
 python3 -m venv venv
 source venv/bin/activate
-pip3 install -r requirements.txt
+pip3 install -r /frontend/requirements.txt
 
 
 python3 frontend -m pytest --cov=app
