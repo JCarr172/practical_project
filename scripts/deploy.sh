8ca9b614f8c155fec40c77c76222f8af7c9f73bd
James Carr <jcarr@qa.com>
2021-06-04 14:28:11 +0000

    Attempt to fix can't find secret key

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,7 @@
 
 ssh docker-manager /bin/bash << 'EOF' 
 export DATABASE_URI=${DATABASE_URI}
-export SECRET=${SECRET}
+export SECRET=jdktdktfljgligljfufufygjh
+echo ${SECRET}
 sudo docker stack deploy --compose-file docker-compose.yaml app
 EOF
