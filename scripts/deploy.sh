49d4910bf8f9fd63410b8837ba0e9934cbabf399
James Carr <jcarr@qa.com>
2021-06-04 10:24:03 +0000

    Debugging jenkinsfile

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,6 @@
 
 ssh docker-manager << EOF 
 export DATABASE_URI=$DATABASE_URI
-ssh docker-manager export SECRET='$SECRET
+ssh docker-manager export SECRET=$SECRET
 ssh docker-manager sudo docker stack deploy --compose-file docker-compose.yaml app
 EOF
